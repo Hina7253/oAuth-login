b444aa382e09d9fb4f0e931d63c97b51b2053d11
Hina <hinakumari.725394@gmail.com>
2026-02-28 22:43:56 +0530

    Add data

--- login/src/main/java/com/example/login/model/User.java
+++ login/src/main/java/com/example/login/model/User.java
@@ -1,4 +1,22 @@
 package com.example.login.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String provider; // github or linkedin
+
+    // getters setters
 }
