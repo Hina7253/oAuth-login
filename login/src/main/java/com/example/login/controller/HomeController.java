732c2ac758da966e1c4fac6ddae095e0d5a18a99
Hina <hinakumari.725394@gmail.com>
2026-03-01 08:52:40 +0530

    update controller

--- login/src/main/java/com/example/login/controller/HomeController.java
+++ login/src/main/java/com/example/login/controller/HomeController.java
@@ -1,4 +1,26 @@
 package com.example.login.controller;
 
+import ch.qos.logback.core.model.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model,
+                       @AuthenticationPrincipal OAuth2User user) {
+
+        model.addAttribute("name",
+                user.getAttribute("login"));
+
+        return "home";
+    }
 }
