4fef995aee987d882887f9247286c5a341374b64
Hina <hinakumari.725394@gmail.com>
2026-03-01 08:03:02 +0530

    add customoauth2userService

---
+++ login/src/main/java/com/example/login/service/CustomOAuth2UserService.java
@@ -0,0 +1,21 @@
+package com.example.login.service;
+
+import com.example.login.repository.User.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class CustomOAuth2UserService
+        extends DefaultOAuth2UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest request)
+        throws OAuth2AuthenticationException{
+
+        OAuth2User oauthUser = super.loadUser(request);
+    }
+}
