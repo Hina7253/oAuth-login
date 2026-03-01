fafea6f540de0ad5d0077393672d6a65f8018db7
Hina <hinakumari.725394@gmail.com>
2026-03-01 08:47:38 +0530

    update SecurityConfig

--- login/src/main/java/com/example/login/config/SecurityConfig.java
+++ login/src/main/java/com/example/login/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package com.example.login.config;
 
+import com.example.login.service.CustomOAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private CustomOAuth2UserService oauth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .loginPage("/login")
+                        .userInfoEndpoint(user ->
+                                user.userService(oauth2UserService)
+                        )
+                        .defaultSuccessUrl("/home", true)
+                );
+
+        return http.build();
+    }
 }
