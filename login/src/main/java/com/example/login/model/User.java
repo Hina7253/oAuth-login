f37b88e0b476f46d67f5617181a33800c184da5d
Hina <hinakumari.725394@gmail.com>
2026-03-08 23:03:08 +0530

    Add @Id annotation to User entity

--- login/src/main/java/com/example/login/model/User.java
+++ login/src/main/java/com/example/login/model/User.java
@@ -10,7 +10,7 @@ import org.springframework.data.annotation.Id;
 @Table(name = "users")
 public class User {
 
-   
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
