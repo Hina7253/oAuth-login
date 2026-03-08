6d05050789400d255de6f66e8ee8ebbcb6967dbc
Hina <hinakumari.725394@gmail.com>
2026-03-08 23:01:33 +0530

    Update User.java

--- login/src/main/java/com/example/login/model/User.java
+++ login/src/main/java/com/example/login/model/User.java
@@ -1,6 +1,6 @@
 package com.example.login.model;
 
-
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
